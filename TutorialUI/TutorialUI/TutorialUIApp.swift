3f7867a888ec3cb16b6dae80bc59a5e2ee5bdb8c
gurjnderSingh <gurjindersingh663@gmail.com>
2023-02-09 08:00:55 +0530

    Initial commit

---
+++ TutorialUI/TutorialUI/TutorialUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialUIApp.swift
+//  TutorialUI
+//
+//  Created by Gurjinder Singh on 09/02/23.
+//
+
+import SwiftUI
+
+@main
+struct TutorialUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
